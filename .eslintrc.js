3521197b53beb5fe3b576789e66e41e8fb4b28d2
AlekseySamokhin <lel.dude01@gmail.com>
2022-12-27 10:47:56 +0300

    add: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,109 @@
+/* eslint-env node */
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+  },
+  extends: ['airbnb-base', 'plugin:@typescript-eslint/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    project: ['./tsconfig.json', 'tsconfig.eslint.json'],
+  },
+  parser: '@typescript-eslint/parser',
+  rules: {
+    'eol-last': ['error', 'always'],
+    'no-plusplus': 'off',
+    'operator-linebreak': [
+      'error',
+      'after',
+      { overrides: { '?': 'before', ':': 'before' } },
+    ],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'prefer-destructuring': 'off',
+    'arrow-parens': ['error', 'always'],
+    'arrow-body-style': 'off',
+    'object-curly-newline': 'off',
+    'no-use-before-define': 'off',
+    'no-shadow': 'off',
+    'space-before-function-paren': [
+      'error',
+      { anonymous: 'always', named: 'never', asyncArrow: 'always' },
+    ],
+    'no-restricted-syntax': 'off',
+    camelcase: 'off',
+    'no-continue': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-unresolved': 'off',
+    'react/display-name': 'off',
+    'consistent-return': 'off',
+    'no-underscore-dangle': ['error', { allowAfterThis: true }],
+    'function-paren-newline': ['error', 'consistent'],
+    'quote-props': 2,
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'import/extensions': 0,
+    'import/no-extraneous-dependencies': 'off',
+    'padded-blocks': ['error', 'never'],
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-param-reassign': [
+      'error',
+      {
+        props: true,
+        ignorePropertyModificationsFor: ['store', 'acc', 'req', 'res'],
+      },
+    ],
+    semi: 'off',
+    '@typescript-eslint/semi': ['error', 'always'],
+    'comma-dangle': 'off',
+    '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
+    '@typescript-eslint/member-delimiter-style': 'error',
+    '@typescript-eslint/no-explicit-any': ['error'],
+    '@typescript-eslint/consistent-generic-constructors': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'variable',
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+      },
+      { selector: 'class', format: ['PascalCase'] },
+      { selector: 'enum', format: ['PascalCase'], suffix: ['ENUM'] },
+      { selector: 'interface', format: ['PascalCase'], prefix: ['I'] },
+      { selector: 'typeAlias', format: ['PascalCase'], suffix: ['Type'] },
+    ],
+    '@typescript-eslint/await-thenable': 'error',
+    indent: 'off',
+    '@typescript-eslint/indent': [
+      'error',
+      2,
+      {
+        ignoredNodes: [
+          'FunctionExpression > .params[decorators.length > 0]',
+          'FunctionExpression > .params > :matches(Decorator, :not(:first-child))',
+          'ClassBody.body > PropertyDefinition[decorators.length > 0] > .key',
+        ],
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': 'error',
+  },
+  overrides: [
+    {
+      files: ['*.description.ts'],
+      rules: {
+        'max-classes-per-file': ['error', { max: 3 }],
+      },
+    },
+    {
+      files: ['./src/db/entities/*'],
+      rules: {
+        'max-len': 'off',
+      },
+    },
+  ],
+};
